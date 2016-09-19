20408da70b524bfb830ef26ca0cf73c607671718
magenx <magenx@users.noreply.github.com>
2016-09-19 12:07:54 +0100

    Create lb_check_droplets.sh

---
+++ lb_check_droplets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# LOAD BALANCER
+# Check running droplets
+
+MAGE_WEB_USER="myshop"
+MAGE_WEB_ROOT_PATH="/home/${MAGE_WEB_USER}/public_html"
+MAGE_ADMIN_EMAIL="alert@myshop.com"
+DROPLETS=$(${MAGE_WEB_ROOT_PATH%/*}/backend.conf)
+
+while /usr/bin/inotifywait -e modify ${MAGE_WEB_ROOT_PATH%/*}/backend.conf; do
+service nginx reload
+echo "IP in Load Balance: ${DROPLETS}" | mail -s "Load Balance droplet reloaded" ${MAGE_ADMIN_EMAIL}
+done
